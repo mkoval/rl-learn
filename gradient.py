8e3a176a32b496f382cd297f20e0ec7f3ad8e36e
Michael Koval <koval.michael@gmail.com>
2011-02-11 01:28:56 -0500

    Finished SGA implementation

--- gradient.py
+++ gradient.py
@@ -1,4 +1,4 @@
-import random
+import math, random, sys
 
 class LQRWorld:
 	def __init__(self, minval, maxval, x0, stddev):
@@ -15,30 +15,71 @@ class LQRWorld:
 		noise  = random.gauss(0, self.stddev)
 		self.x = self.x + action + noise
 		self.x = min(max(self.x, self.minval), self.maxval)
-		return reward
+		return (self.x, reward)
 
 class LQRPolicy:
-	def __init__(w1, w2):
-		self.w1 = w1
-		self.w2 = s2
+	def __init__(weights):
+		self.w1 = weights[0]
+		self.w2 = weights[1]
 	
-	def Update(self, w1, w2):
-		self.w1 = w1
-		self.w2 = w2
+	def Update(self, weights):
+		self.w1 = weights[0]
+		self.w2 = weights[1]
+	
+	def GetPartial(self, i, state, action):
+		mu    = self.w1 * state
+		sigma = 1.0 / (1 + math.exp(-self.w2))
+
+		if i == 0:
+			return (action - mu) * state
+		else:
+			return (pow(action - mu, 2) - pow(sigma, 2)) * (1 - sigma)
 	
 	def ChooseAction(self, state):
 		mu    = self.w1 * state
 		sigma = 1.0 / (1 + math.exp(-self.w2))
-		return random.guass(mu, sigma)
+		return random.gauss(mu, sigma)
+
+def LearnSGA(world, policy, alpha, gamma, baseline, tmax):
+	state   = [ 0.0 ] * tmax
+	reward  = [ 0.0 ] * tmax
+	action  = [ 0.0 ] * tmax
+	D       = [ [ 0.0 ] * tmax for i in range(0, k) ]
+	weights = [ 0.0 ] * 2
+
+	state[0] = world.GetState()
+
+	for t in range(1, tmax):
+		action[t - 1]           = policy.ChooseAction(state[t - 1])
+		state[t], reward[t - 1] = world.DoAction(action[t - 1])
+
+		D[i][t] = [ 0.0 ] * 2
+
+		for i in range(0, k):
+			e[i]    = policy.GetPartial(i, state[t], action[t - 1])
+			D[i][t] = e[i] + gamma * D[i][t - 1]
+			delta_w = (reward[t - 1] - baseline) * D[i][t]
+			weights[i] += alpha * (1 - gamma) * delta_w
+
+		policy.Update(weights)
+	
+	return policy
 
 def main(args):
-	minval  = -4
-	maxval  = +4
-	stddev  = 0.50
-	init_w1 = 0.35 + random.uniform(-0.15, +0.15)
-	init_w2 = 0.00
+	minval   = -4
+	maxval   = +4
+	tmax     = 5000
+	alpha    = 0.01
+	gamma    = 0.90
+	baseline = 0.00
+	stddev   = 0.50
+	init_w1  = 0.35 + random.uniform(-0.15, +0.15)
+	init_w2  = 0.00
 
 	start  = random.uniform(minval, maxval)
 	world  = LQRWorld(minval, maxval, start, stddev)
 	policy = LQRPolicy(init_w1, init_w2)
+	LearnSGA(world, policy, alpha, gamma, baseline, tmax)
 
+if __name == '__main__':
+	sys.exit(main(sys.argv))

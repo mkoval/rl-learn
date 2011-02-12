eaee22488ae4e2269cb0257b236b659272bc07b5
Michael Koval <koval.michael@gmail.com>
2011-02-12 00:55:22 -0500

    gradient: implemented Policy class

--- gradient.py
+++ gradient.py
@@ -2,16 +2,14 @@
 
 from copy import deepcopy
 from math import exp
+from numpy import zeros
 from random import gauss
 
 class Environment:
 	def __init__(self, s0, smin, smax):
-		self.s = s0
+		self.s = deepcopy(s0)
 		self.smin = smin
 		self.smax = smax
-
-	def GetDims(self):
-		return len(self.w)
 
 	def GetState(self):
 		return deepcopy(self.s)
@@ -23,3 +21,30 @@ class Environment:
 		self.s = snew
 		r      = -pow(snew, 2) - pow(a, 2)
 		return (a, r, snew)
+
+class Policy:
+	def __init__(self, w0):
+		self.w = deepcopy(w0)
+
+	def GetParams(self):
+		return deepcopy(self.w)
+
+	def SetParams(self, w):
+		self.w = deepcopy(w)
+
+	def GetDims(self):
+		return len(self.w)
+
+	def GetEligibility(self, s, a):
+		k     = len(self.w)
+		mu    = self.w[0] * s
+		sigma = 1 / (1 + exp(-self.w[1]))
+		e     = zeros(k)
+		e[0]  = (a - mu) * s
+		e[1]  = (pow(a - mu, 2) - pow(sigma, 2)) * (1 - sigma)
+		return e
+
+	def ChooseAction(self, s):
+		mu    = self.w[0] * s
+		sigma = 1 / (1 + exp(-self.w[1]))
+		return gauss(mu, sigma)

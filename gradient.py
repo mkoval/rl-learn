507843cf579d8123af30672e8bed5b3f22ac605a
Michael Koval <koval.michael@gmail.com>
2011-02-12 00:47:04 -0500

    Gradient ascent Environment impl

--- gradient.py
+++ gradient.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python
+
+from copy import deepcopy
+from math import exp
+from random import gauss
+
+class Environment:
+	def __init__(self, s0, smin, smax):
+		self.s = s0
+		self.smin = smin
+		self.smax = smax
+
+	def GetDims(self):
+		return len(self.w)
+
+	def GetState(self):
+		return deepcopy(self.s)
+
+	def DoAction(self, a):
+		snew   = self.s + a
+		snew   = min(max(snew, self.smin), self.smax)
+		a      = snew - self.s
+		self.s = snew
+		r      = -pow(snew, 2) - pow(a, 2)
+		return (a, r, snew)
